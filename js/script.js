d499ad55407cf281c12957df5334006b784ddb3a
NicholasPaparusso <nicopaparusso@gmail.com>
2022-10-13 17:38:37 +0200

    [fix]

--- js/script.js
+++ js/script.js
@@ -7,22 +7,19 @@ const boxCont = document.querySelector('.box-container')
 for(let x = 1; x <= limit ; x++ ){
   const box = document.createElement('div')
   box.className = 'null'
-
-if(x % 3 === 0){
+  if( x % 3 === 0 && x % 5 === 0){
+  box.className  ='fizzbuzz';
+  box.innerText = 'fizzbuzz';
+  }else if(x % 3 === 0){
   box.className = 'fizz';
   box.innerText= "fizz";
 }else if(x % 5 === 0){
   box.className ='buzz';
   box.innerText ='buzz';
-}else if( x % 3 === 0 && x % 5 === 0){
-  box.className  ='fizzbuzz';
-  box.innerText = 'fizzbuzz';
 }else{
   box.innerText = x;
 }
-
-
-
-
 boxCont.append(box);
 }
+
+
